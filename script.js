b66219fe75f0d8326b1d15ff1e7f5f4e8c67d9f9
jSchaefer598 <jaidenschaefer2008@gmail.com>
2025-06-18 09:58:35 -0500

    Final

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ let button = document.querySelector("button");
 
 let resultText = document.querySelector(".result-text");
 
+let word = document.querySelector(".word").value;
+let vibe = document.querySelector(".vibe").value;
+let role = document.querySelector(".role").value;
+let disney = document.querySelector(".disney");
+let understood = document.querySelector(".understood").value;
+let petPeeve = document.querySelector(".pet-peeve").value;
+let strength = document.querySelector(".strength").value;
+
 button.addEventListener("click", function() {
-    let word = document.querySelector(".word").value;
-    let vibe = document.querySelector(".vibe").value;
-    let role = document.querySelector(".role").value;
-    let disney = document.querySelector(".disney");
-    let understood = document.querySelector(".understood").value;
-    let petPeeve = document.querySelector(".pet-peeve").value;
-    let strength = document.querySelector(".strength").value;
     if(word === "Ohana - Family means nobody gets left behind" || word ==="Aloha - Love, peace, hello, and goodbye" && vibe === "Be hurt, but try to understand why" && role === "The wild card" || role === "The joker" && disney === "Lilo giving Pudge the fish a peanut butter sandwich"  && understood === "I act wild, but I care a lot more than I let out" || understood === "I'm not weird - I just see the world differently" && petPeeve === 'People telling me to "calm down"' || petPeeve === 'Being ignored or dismissed' || petPeeve === 'Boring routines or small talk' && strength === 'Empathy - I feel things deeply' || strength === 'Creativity - I make magic out of chaos'){
         resultText.innerHTML = "You got Lilo!";
     } else if(word === "Ohana - Family means nobody gets left behind" || word === "Pono - Righteousness, balance" || word === "Mana - Spiritual energy, power" && vibe ==="Stay calm but replay it in your head for days" || vibe === "Laugh and probably agree"  && role === "The mom friend" || role === "The fixer" && disney === "Jumba gleefully explaining chaos to the council" || disney === "Pleakley panicking in a disguise" && understood === "I'm trying, even if I seem like a mess" || understood === "I'm not careless - I just think differently" && petPeeve === 'People telling me to "calm down"' || petPeeve === 'Plans changing last minute' && strength === 'Resilience - I bounce back, no matter what' || strength === "Loyalty - I'd do anything for the people I love"){
@@ -20,4 +21,5 @@ button.addEventListener("click", function() {
     } else {
         resultText.innerHTML = "There is no Lilo and Stitch character for you.";
     }
+
 });
