c1ff35e16acba3a2dd44e7a5dc50bcb6014a400a
jSchaefer598 <jaidenschaefer2008@gmail.com>
2025-06-20 11:38:25 -0500

    Try Jumba

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ let button = document.querySelector("button");
 
 let resultText = document.querySelector(".result-text");
 
+let word = document.querySelector(".word").value;
+let vibe = document.querySelector(".vibe").value;
+let role = document.querySelector(".role").value;
+let disney = document.querySelector(".disney");
+let understood = document.querySelector(".understood").value;
+let petPeeve = document.querySelector(".pet-peeve").value;
+let strength = document.querySelector(".strength").value;
+
 button.addEventListener("click", function() {
-    let word = document.querySelector(".word").value;
-    let vibe = document.querySelector(".vibe").value;
-    let role = document.querySelector(".role").value;
-    let disney = document.querySelector(".disney");
-    let understood = document.querySelector(".understood").value;
-    let petPeeve = document.querySelector(".pet-peeve").value;
-    let strength = document.querySelector(".strength").value;
     if(word === "Ohana" || word ==="Aloha" && vibe === "Hurt" && role === "Wild card" || role === "Joker" && disney === "Lilo"  && understood === "Wild" || understood === "Weird" && petPeeve === "Calm down" || petPeeve === "Ignored" || petPeeve === "Routines" && strength === "Empathy" || strength === 'Creativity'){
         resultText.innerHTML = "You got Lilo!";
     } else if(word === "Ohana" || word === "Pono" || word === "Mana" && vibe ==="Calm" || vibe === "Laugh"  && role === "Mom" || role === "Fixer" && disney === "Jumba" || disney === "Pleakley" && understood === "Trying" || understood === "Careless" && petPeeve === "Calm down" || petPeeve === "Change plans" && strength === 'Resilience' || strength === "Loyalty"){
@@ -19,6 +20,8 @@ button.addEventListener("click", function() {
         resultText.innerHTML = "You got Stitch!";
     } else if(word === "Aloha" && vibe === "Hurt" || vibe === "Laugh" && role === "Mom" || role === "Fixer" && disney === "Pleakley" && understood === "Weird" || understood === "Trying" && petPeeve === "Calm down" || petPeeve === "Change plans" && strength === "Empathy" || strength === "Loyalty"){
         resultText.innerHTML = "You got Pleakley!";
+    // } else if(word === "Aloha" || word === "Pono" && vibe === "Calm" || vibe === "Laugh" && role === "Wild card" && disney === "Jumba" && understood === "Weird" || understood === "Trying" && petPeeve === "Routines" && strength === "Loyalty" || strength === "Empathy"){
+    //     resultText.innerHTML = "You got Jumba!";
     } else {
         resultText.innerHTML = "There is no Lilo and Stitch character for you.";
     }
